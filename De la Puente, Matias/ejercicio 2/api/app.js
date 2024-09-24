57d3746250d1b29f1b6a89f3d5ec4c8dbb7bae69
Alan Carrizo <alancarrizo1017@gmail.com>
2024-09-24 17:44:58 -0300

    Ejercicio 2: API perimetro completada

--- De la Puente, Matias/ejercicio 2/api/app.js
+++ De la Puente, Matias/ejercicio 2/api/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import { perimetroRouter } from "./perimetro.js"
 
 const app = express()
 const port = 3000
@@ -7,7 +8,7 @@ const port = 3000
 app.use(express.json())
 app.use(cors())
 
-
+app.use(("/perimetro"), perimetroRouter)
 
 app.listen(port, () => {
   console.log(`La aplicación está funcionando en el puerto: ${port}`)
--- De la Puente, Matias/ejercicio 2/api/perimetro.js
+++ De la Puente, Matias/ejercicio 2/api/perimetro.js
@@ -1,1 +1,56 @@
 // Perimetro: La suma de todos los lados
+import express from "express";
+
+export const perimetroRouter = express.Router()
+
+let calculos = [];
+let calcMaxID = 0;
+
+perimetroRouter.get("/", (req, res) => {
+    if (calculos.length === 0) {
+        return res.status(200).send("No se encuentran cálculos previos.");
+    }
+    res.send({ calculos });
+});
+
+perimetroRouter.get("/:id",(req,res)=>{
+    const id = parseInt(req.params.id);
+    const perimetro = calculos.find((calculo)=> calculo.id == id)
+    if(!perimetro){
+        return res.status(404).send("Cálculo no encontrado.")
+    }
+    res.send({ perimetro })
+})
+
+perimetroRouter.post("/",(req,res)=>{
+    const {a, b} = req.body;
+    const data = 
+    {id: ++calcMaxID, 
+    ladoA: a, 
+    ladoB: b, 
+    perimetro: (a*2)+(b*2), 
+    fecha: new Date()}
+
+    calculos.push(data)
+    res.send({ calculos })
+})
+
+perimetroRouter.delete("/:id", (req,res)=>{
+    const id = parseInt(req.params.id);
+    calculos = calculos.filter((calculo)=> calculo.id !== id)
+    res.status(200).send(calculos)
+})
+
+perimetroRouter.put("/:id", (req,res)=>{
+    const id = parseInt(req.params.id);
+    const {a,b} = req.body
+    const data = calculos.find((perimetro)=>perimetro.id == id)
+    if (!data){
+        return res.status(404).send("Cálculo no encontrado.")
+    }
+    data.ladoA = a;
+    data.ladoB = b;
+    data.perimetro = (a*2)+(b*2);
+    data.fecha = new Date();
+    res.status(200).send({data})
+})
