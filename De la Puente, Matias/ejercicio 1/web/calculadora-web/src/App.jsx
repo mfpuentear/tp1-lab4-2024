4e8f785088bb5cc7f24d39afea9f3eade9b506e3
Martin Zalazar <martinzetav@gmail.com>
2024-09-24 19:10:57 -0300

    Se realizo la operacion de POST en el lado del cliente

--- De la Puente, Matias/ejercicio 1/web/calculadora-web/src/App.jsx
+++ De la Puente, Matias/ejercicio 1/web/calculadora-web/src/App.jsx
@@ -1,20 +1,64 @@
 import { useState, useEffect } from "react"
 
 function App() {
+
   const [sumas, setSumas] = useState([]);
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  
+  // Obtenemos listado de sumas cuando se carga por primera vez el componente.
+  const getSumas = async() =>{
+    const response = await fetch("http://localhost:3000/sumas");
+    if(response.ok){
+      const {sumas} = await response.json();
+      setSumas(sumas);
+    }
+  };
+
   useEffect(()=>{
-    const getSumas = async() =>{
-      const response = await fetch("http://localhost:3000/sumas");
-      if(response.ok){
-        const json = await response.json();
-        setSumas(json.sumas);
-      }
-    };
     getSumas();
   }, []);
+
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    // POST http://localhost:3000/sumas (body: a, b).
+    const response = await fetch("http://localhost:3000/sumas", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({a, b})
+    });
+    if(response.ok){
+      // Pedir todas las sumas a la api.
+      // Esta opcion nos trae todos los datos de la api actualizada.
+      // getSumas();
+
+      // Agregar la suma creada devuelta por la api.
+      // Ventaja: es rapido.
+      // Desventaja: Esta opcion no nos devuelve los datos de la api actualizada.
+      const {suma} = await response.json();
+      setSumas([...sumas, suma]);
+    }
+  }
+
+
+
   return (
     <>
       <h1>Sumas</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="a">a:</label>
+          <input type="number" id="a" value={a} onChange={e => setA(parseFloat(e.target.value))}/>
+        </div>
+        <div>
+          <label htmlFor="b">b:</label>
+          <input type="number" id="b" value={b} onChange={e=> setB(parseFloat(e.target.value))} />
+        </div>
+        <button type="submit">Agregar</button>
+      </form>
+
       <ul>
         {sumas.map((suma)=> <li key={suma.id}>{suma.id}: {suma.a} + {suma.b} = {suma.resultado}</li>)}
       </ul>
