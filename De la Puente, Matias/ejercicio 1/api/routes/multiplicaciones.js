4ad0842a9705e123757874e1e6b930d0ac5c9505
Martin Zalazar <martinzetav@gmail.com>
2024-09-25 11:01:24 -0300

    se termino la api para multiplicaciones

--- De la Puente, Matias/ejercicio 1/api/routes/multiplicaciones.js
+++ De la Puente, Matias/ejercicio 1/api/routes/multiplicaciones.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-const multiplicaciones = [
+let multiplicaciones = [
     // {id: 1, a: 2, b: 5, resultado: 10},
     // {id: 2, a: 6, b: 8, resultado: 48},
     // {id: 3, a: 35, b: 25, resultado: 875}
@@ -10,14 +10,14 @@ const multiplicaciones = [
 let multiplicacionesMaxId = 0;
 
 router.get("/", (req, res) => {
-    return res.send(multiplicaciones)
+    return res.send({multiplicaciones})
 });
 
 router.get("/:id", (req, res) => {
     const {id} = req.params;
     const multiplicacion = multiplicaciones.find((multiplicacion) => multiplicacion.id == id);
     if(multiplicacion){
-        return res.send(multiplicacion);
+        return res.send({multiplicacion});
     }else{
         return res.status(404).send({error: `No se encontro una multiplicacion con el id ${id}`}) 
     }
@@ -25,9 +25,38 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
     const {a, b} = req.body;
+    if(a == null || b == null){
+        return res.status(400).send({error: "Debe llenar los campos correspondientes"});
+    }
     const multiplicacion = {id: ++multiplicacionesMaxId, a, b, resultado: a * b, fecha: new Date()};
     multiplicaciones.push(multiplicacion);
-    return res.status(201).send(multiplicacion);
+    return res.status(201).send({multiplicacion});
+})
+
+router.put("/:id", (req, res) =>{
+    const id = parseInt(req.params.id);
+    const { a, b } = req.body;
+    // con find
+    // const multiplicacion = multiplicaciones.find((multiplicacion) => multiplicacion.id == id);
+    // multiplicacion.a = a;
+    // multiplicacion.b = b;
+    // multiplicacion.resultado = a + b;
+    // multiplicacion.fecha = new Date();
+
+    const multiplicacionModificada = { id, a, b, resultado: a * b, fecha: new Date() }
+
+    // con forEach
+    
+    // multiplicaciones.forEach((multiplicacion, index) => {
+    //     if (multiplicacion.id === id) {
+    //         multiplicaciones[index] = multiplicacionModificada;
+    //     }
+    // });
+    
+
+    // con map()
+    multiplicaciones = multiplicaciones.map((multiplicacion) => (multiplicacion.id === id ? multiplicacionModificada : multiplicacion));
+    return res.status(200).send({multiplicacion: multiplicacionModificada});
 })
 
 router.delete("/:id", (req, res)=>{
