c8e1ae142e076d5c9c289d03558b3d049df992b7
Martin Zalazar <martinzetav@gmail.com>
2024-09-25 18:15:22 -0300

    se muestra en la interfaz todos los productos

--- De la Puente, Matias/ejercicio 3/web/verduleria-web/src/App.jsx
+++ De la Puente, Matias/ejercicio 3/web/verduleria-web/src/App.jsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react"
+
 function App() {
+
+  const [productos, setProductos] = useState([]);
+  const [nombre, setNombre] = useState("");
+  const [precio, setPrecio] = useState(0);
+
+  const getProductos = async()=>{
+    const response = await fetch("http://localhost:3000/productos");
+    if(response.ok){
+      const {productos} = await response.json();
+      setProductos(productos);
+    }
+  }
+
+  useEffect(()=>{
+    getProductos();
+  }, []);
+
   return (
     <>
-      
+      <h1>Productos de Verduleria</h1>
+      <ul>
+        {productos.map((prod) => (
+          <li key={prod.id}>{`Id: ${prod.id} / Nombre: ${prod.nombre} / Precio: $${prod.precio}`}</li>
+        ))}
+      </ul>
     </>
   )
 }
