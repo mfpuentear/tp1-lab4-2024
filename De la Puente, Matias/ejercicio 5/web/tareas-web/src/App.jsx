a6fae6e871885e5ce386482b13f753c4c15d7fd3
Martin Zalazar <martinzetav@gmail.com>
2024-09-26 17:14:13 -0300

    se termino con la interfaz de la api de tareas

--- De la Puente, Matias/ejercicio 5/web/tareas-web/src/App.jsx
+++ De la Puente, Matias/ejercicio 5/web/tareas-web/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [tareas, setTareas] = useState([]);
   const [nombre, setNombre] = useState("");
   const [completado, setCompletado] = useState("");
+  const [tareaId, setTareaId] = useState(0);
 
   const getTareas = async() =>{
     const response = await fetch("http://localhost:3000/tareas");
@@ -51,6 +52,42 @@ function App() {
     }
   }
 
+  const handleEdit = (tarea) =>{
+    setNombre(tarea.nombre);
+    setCompletado(tarea.completado);
+    setTareaId(tarea.id);
+  }
+
+  const handleActualizar = async() =>{
+    const response = await fetch(`http://localhost:3000/tareas/${tareaId}`, {
+      method: "PUT",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({nombre, completado})
+    });
+    if(response.ok){
+      getTareas();
+      setNombre("");
+      setCompletado("");
+      setTareaId(0);
+    }else{
+      const errorData = await response.json();
+      alert(errorData.error);
+    }
+  }
+
+  const infoTareas = () =>{
+    const totalTareas = tareas.length;
+    let totalCompletas = 0;
+    let totalIncompletas = 0;
+
+    tareas.forEach((tarea)=>{
+      if(tarea.completado === "Si") totalCompletas++;
+      else totalIncompletas++;
+    })
+    
+    return `Hay ${totalTareas} tarea/s, ${totalCompletas} COMPLETAS y ${totalIncompletas} INCOMPLETAS`;
+  }
+
   return (
     <>
       <h1>TAREAS</h1>
@@ -62,20 +99,31 @@ function App() {
         <div>
           <label>Completada: </label>
           <label htmlFor="si">Si</label>
-          <input type="radio" id="si" value="Si" name="completado" onChange={(e)=>setCompletado(e.target.value)} checked={completado === "Si"}/>
-          <span>//</span>
+          <input type="radio" id="si" value="Si" name="completado" onChange={(e)=>setCompletado(e.target.value)} checked={completado === "Si"} style={{marginRight: "10px"}}/>
           <label htmlFor="no"> No</label>
           <input type="radio" id="no" value="No" name="completado" onChange={(e)=>setCompletado(e.target.value)} checked={completado === "No"}/>
         </div>
-        <button type="submit">Agregar</button>
+        {tareaId == 0 && <button type="submit">Agregar</button>}
       </form>
+      {tareaId !== 0 && (
+        <>
+          <button onClick={()=> handleActualizar()}>Modificar</button>
+          <button onClick={()=>{
+            setNombre("");
+            setCompletado("");
+            setTareaId(0);
+          }}>Cancelar</button>
+        </>
+      )}
       <ol>
         {tareas.map((tarea) =>(
-          <li key={tarea.id}>{`Nombre: ${tarea.nombre} - Completada: ${tarea.completado} `}
-          <button onClick={()=> handleRemove(tarea.id)}>X</button>
+          <li key={tarea.id}>{`Tarea: ${tarea.nombre} - Completada: ${tarea.completado} `}
+          <button onClick={()=> handleEdit(tarea)} disabled={tareaId !== 0}>E</button>
+          <button onClick={()=> handleRemove(tarea.id)} disabled={tareaId !== 0}>X</button>
           </li>
         ))}
       </ol>
+      <h4>{infoTareas()}</h4>
     </>
   )
 }
