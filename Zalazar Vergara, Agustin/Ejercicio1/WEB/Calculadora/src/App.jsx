5982a3d65112866b9296f6b4f79d6eb94e36c8e8
Agustin Zalazar <agustin.zalazar12@gmail.com>
2024-09-27 15:37:49 -0300

    Se agrego Ejercicio1

---
+++ Zalazar Vergara, Agustin/Ejercicio1/WEB/Calculadora/src/App.jsx
@@ -0,0 +1,24 @@
+import Division from "./Division"
+import Multiplicacion from "./Multiplicacion"
+import Resta from "./Resta"
+import Suma from "./Suma"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>CALCULADORA</h1>
+      <Suma/>
+      <hr />
+      <Resta/>
+      <hr />
+      <Multiplicacion/>
+      <hr />
+      <Division/>
+    </>
+  )
+}
+
+export default App
