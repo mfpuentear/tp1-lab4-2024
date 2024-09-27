53e58ae65dbb617e54811f4d9ed50a47ecfab72b
DARIO GALLARDO <dario15799@gmail.com>
2024-09-27 09:59:29 -0300

    api_ejer_2

--- gallardoDario/ejer2/api/myapi/app.js
+++ gallardoDario/ejer2/api/myapi/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { perimetrosRouter } from "./perimetro.js";
-
+import { superficieRouter } from "./superficie.js";
 const app = express();
 
 const port = 3000;
@@ -8,10 +8,11 @@ const port = 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Worlddesdes perimetro!");
 });
 app.use(express.json());
 app.use("/perimetros", perimetrosRouter);
+app.use("/superficies", superficieRouter);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
