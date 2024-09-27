2a5fcccf4a992c5e754127532ce8183a1657b2ef
Ramon Barrera <rebcontacto@gmail.com>
2024-09-27 18:16:00 -0300

    Avance en el ejercicio 4

--- Barrera Ramon/ejercicio 4/web/src/App.jsx
+++ Barrera Ramon/ejercicio 4/web/src/App.jsx
@@ -23,6 +23,10 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+  if (sumaId){
+    await modificarAlumno(sumaId)
+    setSumaId(0)
+  } else {
     const res = await fetch("http://localhost:3000/alumnos", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -30,16 +34,51 @@ function App() {
     });
     if (res.ok) {
       const { data } = await res.json();
-      getAlumnos([...nombres, data])
-      setNombre("")
-      setNota1("")
-      setNota2("")
-      setNota3("")
+      set([...nombres, data])
+  }
+  setNombre("")
+  setNota1("")
+  setNota2("")
+  setNota3("")
     }
   };
 
+  const eliminarAlumno = async (id) => {
+    if (confirm("¿Quiere borrar Alumno?")) {
+      const res = await fetch(`http://localhost:3000/alumnos/${id}`, {
+        method: "DELETE",
+      });
 
+      if (res.ok) {
+        setNombres(nombres.filter((alumn) => alumn.id !== id));
+      }
+      
+    }
+  };
 
+  // const modificarAlumno = async (id) => {
+  //   const res = await fetch(`http://localhost:3000/alumnos/${id}`, {
+  //     method: "PUT",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ nombre, nota1, nota2, nota3}),
+  //   });
+
+  //   if (res.ok) {
+  //     const { data } = await res.json();
+  //     setNombres(nombres.map((t) => (t.id === data.id ? data : t)));
+  //   }
+  // };
+    
+  // const modificarAlumn = (alumno) => {
+
+  //   setNombre(alumno.nombre);
+  //   setNota1(alumno.nota1);
+  //   setNota2(alumno.nota2);
+  //   setNota3(alumno.nota3);
+  //   setSumaId(alumno.id);
+    
+  // }
+    
   return (
     <>
     <form action="" onSubmit={handleSubmit}>
@@ -61,7 +100,7 @@ function App() {
         const promedio = (parseInt(ele.nota1) + parseInt(ele.nota2) + parseInt(ele.nota3)) / 3
         const condicion = promedio < 6 ? "Reprobado"  : promedio >= 8  ? "Promocionado" : "Aprobado";
         return ( <li key={ele.id}> {ele.nombre} | Notas: {ele.nota1} - {ele.nota2} - {ele.nota3} | <br />
-        Promedio: {promedio.toFixed(2)} | Condición: {condicion} </li> )
+        Promedio: {promedio.toFixed(2)} | Condición: {condicion} <button onClick={() => eliminarAlumno(ele.id)}>Eliminar</button> <button onClick={() => modificarAlumno(ele.id)}>Editar</button> </li> )
       })}
     </ul>
     </>
