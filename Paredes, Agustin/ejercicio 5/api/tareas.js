58af91be758eaa1b93c00aa20e82b24410b85037
Agustin Paredes <paredes.skate@gmail.com>
2024-09-27 22:33:11 -0300

    se sube actividad 5. La actividad 3 no me permite subirla de ninguna manera, el archivo esta roto, se lo intento mandar por email profe, disculpe

---
+++ Paredes, Agustin/ejercicio 5/api/tareas.js
@@ -0,0 +1,71 @@
+import express from "express"
+
+const tareasRouter = express.Router()
+let tareas = []
+let id = 0
+
+tareasRouter.get("/", (req, res) => {
+    res.json(tareas)
+})
+
+tareasRouter.post("/", (req, res) => {
+    const { nombre, completado } = req.body
+
+    if (!nombre) {
+    res.status(200).json({ error: "La tarea debe tener un nombre" })
+    return
+    }
+
+    if (tareas.some((tarea) => tarea.nombre === nombre)) {
+    res.status(200).json({ error: "Ya existe una tarea con el mismo nombre" })
+    return
+    }
+
+    const tarea = {
+    id,
+    nombre,
+    completado
+    }
+
+    tareas.push(tarea)
+    res.status(201).json(tarea)
+    id++
+})
+
+tareasRouter.put("/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const { nombre, completado } = req.body
+
+    const tarea = tareas.find((t) => t.id === id)
+    if (!tarea) {
+    res.status(404).json({ error: "Tarea no encontrada" })
+    return
+    }
+
+    if (!nombre) {
+    res.status(400).json({ error: "La tarea debe tener un nombre" })
+    return
+    }
+
+    if (tareas.some((tarea) => tarea.nombre === nombre && tarea.id !== id)) {
+    res.status(400).json({ error: "Ya existe una tarea con el mismo nombre" })
+    return
+    }
+
+    tarea.nombre = nombre
+    tarea.completado = completado
+    res.json(tarea)
+})
+
+tareasRouter.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const index = tareas.findIndex((t) => t.id === id)
+    if (index === -1) {
+    res.status(404).json({ error: "Tarea no encontrada" })
+    return
+    }
+    tareas.splice(index, 1)
+    res.sendStatus(204)
+})
+
+export default tareasRouter
