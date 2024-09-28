ded2b0d06f777099217b92c7c3beaf1ed15ff8ec
Cecilia Bolado <ceciliaboladodevita@gmail.com>
2024-09-27 21:47:24 -0300

    Agrego ejercicio 4

---
+++ Bolado, Cecilia/ejercicio 4/api/alumnos.js
@@ -0,0 +1,91 @@
+import express from "express"
+
+const rutas = express()
+let alumnos = []
+let id = 0
+
+rutas.get("/", (req, res) => {
+  res.send(alumnos)
+})
+
+rutas.post("/", (req, res) => {
+  const { nombre, notas } = req.body
+
+  if (!nombre || !notas || notas.length !== 3) {
+    res.status(200).json({ error: "Datos incompletos del alumno" })
+    return
+  }
+
+  if (alumnos.some((alumno) => alumno.nombre === nombre)) {
+    res.status(200).json({ error: "Ya existe un alumno con el mismo nombre" })
+    return
+  }
+
+  if (alumnos.some((nota) => nota < 0 || nota > 10)) {
+    res.status(200).json({ error: "Las notas deben ser mayores a 0 y menores a 10" })
+    return
+  }
+
+  const promedio = notas.reduce((acc, nota) => acc + nota, 0) / notas.length
+
+  const alumno = {
+    id,
+    nombre,
+    notas,
+    promedio,
+  }
+
+  alumnos.push(alumno)
+  res.status(201).json(alumno)
+  id++
+})
+
+rutas.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id)
+  const { nombre, notas } = req.body
+
+  const alumno = alumnos.find((a) => a.id === id)
+  if (!alumno) {
+    res.status(404).json({ error: "Alumno no encontrado" })
+    return
+  }
+
+  if (!nombre || !notas || notas.length !== 3) {
+    res.status(400).json({ error: "Datos incompletos" })
+    return
+  }
+
+  if (alumnos.some((a) => a.nombre === nombre && a.id !== id)) {
+    res.status(400).json({ error: "Ya existe un alumno con el mismo nombre" })
+    return
+  }
+
+  if (alumnos.some((nota) => nota < 0)) {
+    res.status(400).json({ error: "Las notas deben ser mayores a cero" })
+    return
+  }
+
+  alumno.nombre = nombre
+  alumno.notas = notas
+  alumno.promedio = (
+    notas.reduce((acc, nota) => acc + nota, 0) / notas.length
+  ).toFixed(2)
+
+  alumnos = alumnos.map((a) => (a.id === id ? alumno : a))
+  res.json(alumno)
+})
+
+rutas.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id)
+  const alumno = alumnos.find((a) => a.id === id)
+
+  if (!alumno) {
+    res.status(404).json({ error: "Alumno no encontrado" })
+    return
+  }
+
+  alumnos = alumnos.filter((a) => a.id !== id)
+  res.json({ mensaje: "Alumno eliminado" }).status(410)
+})
+
+export default rutas
