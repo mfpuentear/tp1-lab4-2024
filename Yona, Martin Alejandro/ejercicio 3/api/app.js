fa5cc3a73f0a19e1d6350e9751891b8d5f248cc3
martinalejandroyona <martinalejandroyona@gmail.com>
2024-09-28 22:05:09 -0300

    Subida ejercicio 3

---
+++ Yona, Martin Alejandro/ejercicio 3/api/app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import { productosRouter } from './productos.js';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/', (req , res) =>{
+    res.send('Hola mundo')
+})
+app.use('/productos',productosRouter)
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`)
+})
